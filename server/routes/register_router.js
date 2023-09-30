4c3a28f68e5fdad2a2b217e9889089b9232db5f2
joaosaraiva <joaosaraiva.s.js@gmail.com>
2023-09-30 15:02:47 +0100

    Fixes

--- server/routes/register_router.js
+++ server/routes/register_router.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const db = require("../db/db");
 
 router.post("/", async (req, res) => {
   const { username, email, password, profile_pic } = req.body;
