c67311d419875b268d22b4cc1c4b428478b47fec
JoaoSaraiva1 <48354004+JoaoSaraiva1@users.noreply.github.com>
2023-10-02 17:35:34 +0100

    Implementa Select para sorting (#3)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/jsx-pascal-case */
 import React, { useState, useEffect } from "react";
-import Task_Card from "../components/Task_Card";
-import Task_Form from "../components/Task_Form";
+import TaskCard from "../components/Task_Card";
+import TaskForm from "../components/Task_Form";
 import AddBoxIcon from "@mui/icons-material/AddBox";
+import TaskSorting from "../components/Task_Sorting";
+import { sortTasks } from "../utils/Sort_Tasks";
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
   const [categories, setCategories] = useState([]);
   const [showForm, setShowForm] = useState(false);
+  const [sortOption, setSortOption] = useState("uncompletedFirst");
 
   useEffect(() => {
     fetch("http://localhost:5000/tasks")
@@ -18,6 +21,12 @@ const Home = () => {
       .then((response) => response.json())
       .then((data) => setCategories(data));
   }, []);
+
+  const handleSort = (selectedSortOption) => {
+    setSortOption(selectedSortOption);
+  };
+
+  const sortedTasks = sortTasks(tasks, sortOption);
 
   const handleToggleForm = () => {
     setShowForm(!showForm);
@@ -30,6 +39,7 @@ const Home = () => {
   return (
     <div>
       <h1>Task Manager</h1>
+      <TaskSorting onSort={handleSort} />
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         <div
           className="add-card"
@@ -53,8 +63,8 @@ const Home = () => {
           <AddBoxIcon style={{ fontSize: 50 }} />
           <p style={{ margin: "8px", fontWeight: "bold" }}>Add a new task</p>
         </div>
-        {tasks.map((task) => (
-          <Task_Card key={task.id} task={task} categories={categories} />
+        {sortedTasks.map((task) => (
+          <TaskCard key={task.id} task={task} categories={categories} />
         ))}
       </div>
       <div>
@@ -66,7 +76,7 @@ const Home = () => {
         </ul>
       </div>
       {showForm && (
-        <Task_Form
+        <TaskForm
           isOpen={showForm}
           onAddTask={handleAddTask}
           onClose={handleToggleForm}
